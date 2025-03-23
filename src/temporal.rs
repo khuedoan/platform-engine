5603f92ebc4c3630d7f1789055aa047fff158cb3
Khue Doan <mail@khuedoan.com>
2025-03-23 15:01:10 +0700

    feat: add env var to set Temporal URL

--- src/temporal.rs
+++ src/temporal.rs
@@ -6,7 +6,8 @@ use tracing::info;
 
 pub async fn get_client() -> Result<RetryClient<Client>> {
     info!("connecting to Temporal");
-    let server_options = sdk_client_options(Url::parse("http://localhost:7233")?).build()?;
+    let temporal_url = std::env::var("TEMPORAL_URL").unwrap_or("http://localhost:7233".to_string());
+    let server_options = sdk_client_options(Url::parse(&temporal_url)?).build()?;
     let client = server_options.connect("default", None).await?;
     info!("connected to Temporal");
 
